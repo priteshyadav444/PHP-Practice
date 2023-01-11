ba91e146e833920a16409b22a85f3c85c6361fd5
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-01-11 17:38:01 +0530

    error handler method added with mapped -> message

--- ControlingStatement/ValidateProgram/Validate.php
+++ ControlingStatement/ValidateProgram/Validate.php
@@ -1,32 +1,34 @@
 <?php
 class Validate
 {
-    // check input is int or not 
-    // params: mixed
-    // return type : boolean
+    // Validate input is int or not 
+    // @params: mixed
+    // @return type : boolean
     public static function isInt($input): bool
     {
         $intValue = intval($input);
         return ($input == $intValue);
     }
-    // check input is flaot or not 
-    // params: mixed
-    // return type : boolean
+
+    // Validate input is flaot or not 
+    // @params: mixed
+    // @return type : boolean
     public static function isfloat($input): bool
     {
         $intValue = floatval($input);
         return ($input == $intValue);
     }
-    // check input is string or not 
-    // params: mixed
-    // return type : boolean
+
+    // Validate input is string or not 
+    // @params: mixed
+    // @return type : boolean
     public static function isString($input): bool
     {
         return is_string($input);
     }
-    // return integer, float or bool when invalid data 
-    // params: mixed
-    // return type : int | float | bool
+    // Return integer, float or bool when invalid data 
+    // @params: mixed
+    // @return type : int | float | bool
     public static function getInt($input): int | float | bool
     {
         if (self::isInt($input)) {
@@ -36,9 +38,10 @@ class Validate
         }
         return false;
     }
-    // return string value or boolen (false) when invalid data
-    // params: mixed
-    // return type : string | bool
+
+    // Return string value or boolen (false) when invalid data
+    // @params: mixed
+    // @return type : string | bool
     public static function getString($input): string | bool
     {
         if (self::isString($input) && !empty($input) && !self::isInt($input)) {
@@ -47,9 +50,10 @@ class Validate
         }
         return false;
     }
-    // extract only int from the the input
-    // params: mixed
-    // return type : int | float
+
+    // Extract only int from the the input
+    // @params: mixed
+    // @return type : int | float
     public function extractInteger($input): int | float
     {
         // if input is is already integer
@@ -57,7 +61,7 @@ class Validate
         if (self::isInt($input)) return $input;
         $result = 0;
         for ($i = 0; $i < strlen($input); $i++) {
-            // check for floating value
+            // Validate for floating value
             // if (isset($input[$i]) && $input[$i] == ".")
             //     break;
 
@@ -67,9 +71,10 @@ class Validate
         }
         return $result;
     }
+
     // extract only sting charecter from the the input
-    // params: mixed
-    // return type : string
+    // @params: mixed
+    // @return type : string
     public function extractString($input)
     {
         if ($input == null) return false;
@@ -85,15 +90,16 @@ class Validate
         return $result;
     }
 
-    // helper function of validateInput check pass datattypes untill data match to $dataTypes
-    // params: $functionname : particular function for checking datatypes , $input : input data , $dataTypes : expected
-    // return type : params : $dataTypes 
+    // helper function of validateInput Validate pass datattypes untill data match to $dataTypes
+    // @params: $functionname : particular function for Validateing datatypes , $input : input data , $dataTypes : expected
+    // @return type : @params : $dataTypes 
     private function handleInput($functionName, $input, $dataTypes)
     {
         $result = Validate::$functionName($input);
         do {
             if ($result === false) {
                 self::__displayError($dataTypes);
+                self::errorHandler('INVALID_DATATYPE', $dataTypes);
                 $result = Validate::$functionName(readline("Enter Input Again :\n"));
             } else {
                 break;
@@ -101,9 +107,10 @@ class Validate
         } while (true);
         return $result;
     }
+
     // validate input unless pass datatypes doest match on console application
-    // params: $input : passed data, $dataTypes : pass datatypes that  expected 
-    // return type : params : $dataTypes
+    // @params: $input : passed data, $dataTypes : pass datatypes that  expected 
+    // @return type : @params : $dataTypes
     public function validateInput($input, $dataTypes)
     {
         if (isset($input) && $dataTypes == 'int' || $dataTypes == 'string') {
@@ -112,15 +119,46 @@ class Validate
             return $result;
         }
     }
-    // display error message with expected Data Types 
-    // params: $dataTypes : pass datatypes that  expected 
-    // return type : void
-    private function __displayError($dataTypes)
+
+    // display error message o fmapped Error code
+    // @params: $errorCode : error Code of Error Message
+    // @return type : void
+    public function errorHandler($errorCode, $dataTypes = null)
     {
-        echo "Enter Valid Data Expected ($dataTypes)\n";
+        $errorMessage = match ($errorCode) {
+            'INVALID_DATATYPE' => 'Enter Valid Data Expected',
+            'INVALID_OPTION' => 'Enter option is In Valid',
+            'NO_DATA_FOUND' => 'No Record Found!!',
+            'DATABASE_EMPTY' => 'Dataset is Empty!!!!',
+            default => "Unexpected Error",
+        };
+
+        if ($errorCode == 'INVALID_DATATYPE' && $dataTypes != null) {
+            self::__displayError($errorMessage, $dataTypes);
+            return;
+        }
+        self::__displayError($errorMessage);
     }
-    public function echoit($msg)
+
+    // display error message with expected Data Types 
+    // @params: $dataTypes : pass datatypes that  expected 
+    // @return type : void
+    private function __displayError($message, $dataTypes = null)
     {
-        echo "$msg\n\n";
+        $message = $dataTypes == null ? $message : $message . " Expectded ($dataTypes)";
+        self::echoit($message);
+    }
+    // echo with two line
+    public function echoit($msg, $newLine = 2)
+    {
+        if ($newLine < 1) {
+            echo "$msg \n";
+            return;
+        }
+
+        $temp = "";
+        for ($i = 1; $i <= $newLine; $i++)
+            $temp .= "\n";
+        echo $msg . $temp;
     }
 }
