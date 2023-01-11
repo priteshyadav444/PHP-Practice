5222e7982cb66124a61693d86b835c0de0e9cabc
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-01-11 13:23:15 +0530

    LOCAL AND DB FILES sepreted

---
+++ ControlingStatement/StudentMenuDrivenConsole-LOCAL/Student.php
@@ -0,0 +1,64 @@
+<?php
+include_once '../ValidateProgram/Validate.php';
+abstract class Person extends Validate
+{
+    protected $name;
+    protected $address;
+}
+
+class Student extends Person
+{
+    private $studentId;
+    public $name;
+    public $address;
+    const IdPrefix = 'MCA';
+    private static $idCounter = 1;
+
+    public function getData()
+    {
+        echo "\n--------------------------------------- \n";
+        echo "Student Id :" . $this->getId() . "\n";
+        echo "Student Name :" . $this->getName() . "\n";
+        echo "Student Address :" . $this->getAddress() . "\n";
+        echo "--------------------------------------- \n\n";
+    }
+    private function setCustomer($name, $address)
+    {
+        $this->name = $name;
+        $this->address = $address;
+        $this->studentId = self::makeNewId();
+        self::$idCounter++;
+    }
+    public function makeNewId()
+    {
+        $id = self::getNewId();
+        $newId = self::IdPrefix . $id;
+        return $newId;
+    }
+    public  function readData()
+    {
+        $name = parent::validateInput(parent::extractString(readLine("Enter Name : ")), 'string');
+        $address =  parent::validateInput(readLine("Enter Address : "), 'string');
+        $this->setCustomer($name, $address);
+    }
+    public function getNewId()
+    {
+        return self::$idCounter;
+    }
+    private function getId()
+    {
+        return $this->studentId;
+    }
+    private function getName()
+    {
+        return $this->name;
+    }
+    private function getAddress()
+    {
+        return $this->address;
+    }
+    public function checkId($id)
+    {
+        return (strtolower($id) == strtolower($this->getId()));
+    }
+}
---
+++ ControlingStatement/StudentMenuDrivenConsole-LOCAL/StudentCollection.php
@@ -0,0 +1,91 @@
+<?php
+include_once './Student.php';
+include_once '../ValidateProgram/Validate.php';
+
+class StudentCollection extends Validate
+{
+    private $objects = array();
+    public function createStudent()
+    {
+        $student = new Student;
+        $student->readData();
+        array_push($this->objects, $student);
+    }
+    public function showAllStudent()
+    {
+        if (count($this->objects) == 0) {
+            echo "No Record Found !! \n\n";
+            return;
+        }
+        foreach ($this->objects as $key => $obj) {
+            $currentObject = $obj;
+            $currentObject->getData();
+        }
+    }
+
+    public function deleteStudent()
+    {
+        if (count($this->objects) == 0) {
+            parent::echoit("Dataset is Empty!!");
+            return;
+        }
+        $id = readLine("Enter Id : ");
+        $flag = false;
+        foreach ($this->objects as $key => $obj) {
+            $currentObject = $obj;
+            if ($currentObject->checkId($id)) {
+                unset($this->objects[$key]);
+                $flag = true;
+                echo "Records Deleted \n\n";
+                self::showAllStudent();
+            }
+        }
+        if ($flag === false) {
+            echo "Record Not Found!!! \n\n";
+        }
+    }
+
+    public function searchStudent()
+    {
+        if (count($this->objects) == 0) {
+            parent::echoit("Dataset is Empty!!");
+            return;
+        }
+        $id = readLine("Enter Id To Search: ");
+        $flag = false;
+        foreach ($this->objects as $key => $obj) {
+            $currentObject = $obj;
+            if ($currentObject->checkId($id)) {
+                parent::echoit("\nRecords Found !!");
+                $currentObject->getData();
+                $flag = true;
+            }
+        }
+        if ($flag === false) {
+            echo "Record Not Found!!! \n\n";
+        }
+    }
+
+    public function updateStudent()
+    {
+        $flag = false;
+        if (count($this->objects) == 0) {
+            parent::echoit("Dataset is Empty!!");
+            return;
+        }
+        $id = readLine("Enter Id to Update: ");
+        foreach ($this->objects as $key => $obj) {
+            $currentObject = $obj;
+            if ($currentObject->checkId($id)) {
+                $currentObject->name = parent::validateInput(parent::extractString(readLine("Enter Name : ")), 'string');
+                $currentObject->address =  parent::validateInput(readLine("Enter Address : "), 'string');
+                $flag = true;
+                parent::echoit("Record Updated!!!");
+                self::showAllStudent();
+            }
+        }
+        if ($flag === false) {
+            echo "Record Not Found!!! \n\n";
+        }
+    }
+}
