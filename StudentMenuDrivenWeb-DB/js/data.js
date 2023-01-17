a98531108a49512684261c9a82fe017721c78213
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-01-17 15:00:40 +0530

    append in table fixed

--- StudentMenuDrivenWeb-DB/js/data.js
+++ StudentMenuDrivenWeb-DB/js/data.js
@@ -6,25 +6,24 @@ $(document).ready(function () {
 
         function loadData() {
             var xhttp = new XMLHttpRequest();
+            $("#studentTable tbody").empty();
             xhttp.onreadystatechange = function () {
                 if (this.readyState == 4 && this.status == 200) {
                     const response = JSON.parse(this.responseText);
                     const data = response.data;
-                    console.log(data);
-                    
                     data.forEach(data => {
-                        var newRowContent = "<tr><td>"+ data.studentId +"</td><td>MCA"+ data.studentId +"</td><td>"+ data.name +"</td><td>"+ data.address +"</td> <td> <form action='update.php' method='post'> <input type='hidden' value= name='uid'> <button type='submit' class='btn btn-primary'>Update</button> </form></td></td> <td> <form action='update.php' method='post'> <input type='hidden' value= name='uid'> <button type='submit' class='btn btn-danger'>Delete</button> </form></td></tr>";
-                        $("#studentTable tbody").append(newRowContent); 
-
+                        console.log(data);
+                        var newRowContent = "<tr><td>" + data.studentId + "</td><td>MCA" + data.studentId + "</td><td>" + data.name + "</td><td>" + data.address + "</td> <td> <form action='update.php' method='post'> <input type='hidden' value= name='uid'> <button type='submit' class='btn btn-primary'>Update</button> </form></td></td> <td> <form action='update.php' method='post'> <input type='hidden' value= name='uid'> <button type='submit' class='btn btn-danger'>Delete</button> </form></td></tr>";
+                        $("#studentTable tbody").append(newRowContent);
                     });
-        
+
                 }
             };
-            const apiUrl = 'http://192.168.1.126//programs/ControlingStatement/StudentMenuDrivenWeb-DB/StudentCollection.php';
+            const apiUrl = 'http://192.168.1.126//programs/StudentMenuDrivenWeb-DB/StudentCollection.php';
             xhttp.open("GET", apiUrl, true);
             xhttp.send();
         }
         loadData();
     })
-    loadData();
+
 });
---
+++ Variables/Job.php
@@ -0,0 +1,7 @@
+
+<?php
+  
+
+  echo "Last time of file change: " . date("F d Y H:i:s.", filemtime("./flaws.php"));
+    
+  ?>
