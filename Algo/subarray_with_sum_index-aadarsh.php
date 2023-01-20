50b0901adf4f9ce50e9f6c5f0272a69d69fee056
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-01-20 19:31:40 +0530

    subarray with sum 2 algo added

---
+++ Algo/subarray_with_sum_index-aadarsh.php
@@ -0,0 +1,27 @@
+<?php
+    $arr = [12,3,7,1,4,6,9,2,45,6,12];
+    $size = count($arr);
+    $s = 11;
+    $sum = 0;
+    $j = 0;
+    function checkAdd(&$arr, &$j,&$size,&$s,&$sum){
+        if($j < $size){
+            for($i = $j; $i < $size; $i++){
+                $sum += $arr[$i]; 
+
+                if($sum == $s){
+                    echo "\n".$j.' '.$i;
+                    exit();
+                }
+            }
+            $j++;
+            $sum = 0;
+            checkAdd($arr, $j,$size,$s,$sum);
+        }
+        else{
+            echo 'no';
+        }
+    }
+    checkAdd($arr, $j,$size,$s,$sum);
+    
+?>
