497aaf043ee916adf694aff2386c282194d7bdfd
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-01-20 14:53:52 +0530

    subarray with sum algo added

---
+++ Array/Algo/subarray_with_sum_index.php
@@ -0,0 +1,28 @@
+<?php
+function solve($inputArray, $length, $sum)
+{
+    $start = 0;
+    $end = 0;
+    $currentsum = $inputArray[$start];
+    while ($start < $length && $end < $length) {
+        if ($currentsum == $sum) {
+            echo "Start: $start End : $end \n";
+            break;
+        }
+        if ($currentsum > $sum) {
+            $currentsum = 0;
+            $currentsum += $inputArray[++$start];
+            $end = $start;
+        } else {
+            if ($end < $length - 1) {
+                $currentsum += $inputArray[++$end];
+            } else if ($currentsum < $sum) {
+                break;
+            }
+        }
+        echo "Current Sum: $currentsum (Start : $start And End : $end)\n";
+    }
+}
+
+$arr = [9, 1, 8, 3, 5, 2, 1, 3, 2];
+solve($arr, count($arr), 23);
