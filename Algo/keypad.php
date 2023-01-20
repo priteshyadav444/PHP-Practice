0508874c4b94edb98619cd54cb3986e26c550004
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-01-20 19:30:57 +0530

    kaypad program added

---
+++ Algo/keypad.php
@@ -0,0 +1,13 @@
+<?php
+$key1 = [1, 'A', 'B', 'C'];
+$key2 = [2, 'D', 'E', 'F'];
+$key3 = [3, 'G', 'H', 'I'];
+$key4 = [4, 'J', 'K', 'L'];
+$key5 = [5, 'M', 'N', 'O'];
+$key6 = [6, 'P', 'Q', 'R', 'S'];
+$key7 = [7, 'T', 'U', 'V'];
+$key1 = [8, 'W', 'X', 'Y', 'Z'];
+
+for ($i = 0; $i < strlen($input); $i++) {
+    array_search($key1[$i], $userdb);
+}
