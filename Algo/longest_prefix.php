91869fc8fea96e658a4e8c7b5df5ca53ce9f9aa0
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-01-24 19:06:46 +0530

    longest prefix from string in array

---
+++ Algo/longest_prefix.php
@@ -0,0 +1,32 @@
+<?php
+function solve($inputArray, $length)
+{
+    $firstString = $inputArray[0];
+    $prefixLength = strlen($firstString);
+
+    for ($i = 1; $i < $length; $i++) {
+        $currentString = $inputArray[$i];
+        $currentLength = 0;
+        for ($j = 0; $j < strlen($currentString) and $j < strlen($firstString); $j++) {
+            if ($currentString[$j] == $firstString[$j]) {
+                echo "$currentString[$j] CC ";
+                $currentLength++;
+            } else {
+                break;
+            }
+        }
+        if ($prefixLength > $currentLength) $prefixLength = $currentLength;
+    }
+
+    if ($prefixLength == 0) return -1;
+    return substr($firstString, 0, $prefixLength);
+}
+
+$arr = [
+    'dmlrpjyatcoqotxzplqmlptaipczhlikztcofaoaedruyqundkzqatqkkvjrgucineyugnxmsohsgdfmngcpbvamqldyfhgvnfrv',
+    'oioerglunzjvbzxwblooqnuytrnyijuxtibkoogdppzrqyptjeizrezmvnnfyherqidgyjkoyjfrhwkscsrvytivivbgcfxupab',
+    'llclwjcdfpvijodijndriexnmwhbyiplvtxrcbwkqtsaixitn',
+    'lvskkgjujheztaustxtqhklbkvyupnhajbmvhvprfusawmspjlhsvtthouddhlfsmsqwpfpubhuzvmrhaazx'
+];
+
+print_r(solve($arr, count($arr)));
