81a3302a0db56a1a3e6cb1f32a843789e5517e50
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-01-24 15:28:35 +0530

    test cases added

--- Algo/minimum_jump.php
+++ Algo/minimum_jump.php
@@ -8,32 +8,36 @@ function noOfJumps($arr, $length)
         $currJumpLength = $arr[$i];
         $nextMaxJump = 0;
         $ans++;
+        echo "Current {$currJumpLength} and Ans : {$ans} and i : {$i}\n";
 
+        if (($i + $currJumpLength + 1) >= $length) {
+            break;
+        }
         $subarrayindex = 1;
 
         while ($currJumpLength--) {
             if (isset($arr[$i + $subarrayindex]) > 0) {
                 if ($nextMaxJump < $arr[$i + $subarrayindex]) {
                     $nextMaxJump = $arr[$i + $subarrayindex];
-                    $i = $i + $subarrayindex;
-                    $subarrayindex++;
                 }
-            } else {
+            } elseif ($i  >= $length - 1) {
                 $i = $i + $subarrayindex;
                 $nextMaxJump = $arr[$length - 1];
                 break;
             }
+            $i = $i + $subarrayindex;
+            $subarrayindex++;
+            echo "Next {$nextMaxJump} and Ans : {$ans} and i : {$i}\n";
         }
-
-
-        echo "Next {$nextMaxJump} and Ans : {$ans} and i : {$i}\n";
         if ($nextMaxJump == 0) return -1;
     }
     return $ans;
 };
 
-$arr = [1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9];
-// echo "\n Ans : " . noOfJumps($arr, count($arr));
-
+// $arr = [1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9];
 $arr = [1, 2, -1, 1, 3];
+// $arr = [1, 3, -1, 1, 0]; 
+// $arr = [1, 4, 3, 2, 6, 7];
+
+
 echo "\n Ans : " . noOfJumps($arr, count($arr));
