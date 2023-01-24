f3becb088ffaffef513424efea44cde2ea89502a
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-01-24 19:06:27 +0530

    is anagram program

---
+++ Algo/is_anagram.php
@@ -0,0 +1,30 @@
+<?php
+function solve($string1, $string2)
+{
+    if (strlen($string1) != strlen($string2)) return -1;
+    $map = array();
+
+    for ($i = 0; $i < strlen($string1); $i++) {
+        if (!isset($map[$string1[$i]])) {
+            $map[$string1[$i]] = 1;
+        } else {
+            $map[$string1[$i]]++;
+        }
+    }
+    for ($i = 0; $i < strlen($string2); $i++) {
+        if (!isset($map[$string2[$i]])) {
+            return -1;
+        } else {
+            $map[$string1[$i]]--;
+        }
+    }
+    foreach ($map as $value) {
+        if ($value > 0) {
+            return false;
+        }
+    }
+    return 1;
+}
+
+
+print_r(solve('tgzonrrftzq', 'tqzzrtnrftg'));
