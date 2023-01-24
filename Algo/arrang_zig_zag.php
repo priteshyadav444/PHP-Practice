942a57fc3de07d681f152c8aa7ac8c61e1f4b672
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-01-24 12:26:30 +0530

    Zigzag program ( ar[0]<ar[1] >ar[2]< ar[3]>ar[4] )

---
+++ Algo/arrang_zig_zag.php
@@ -0,0 +1,28 @@
+<?php
+function swap(&$first, &$second)
+{
+    $temp = $first;
+    $first = $second;
+    $second = $temp;
+}
+function sortZigZag(&$arr, $length)
+{
+    $greater = true;
+    for ($i = 1; $i < $length - 1; $i++) {
+        if ($greater) {
+            if (!($arr[$i] > $arr[$i - 1] && $arr[$i] > $arr[$i + 1])) 
+                swap($arr[$i], $arr[$i + 1]);
+            
+        } else {
+            if (!($arr[$i] < $arr[$i - 1] && $arr[$i] < $arr[$i + 1])) 
+                swap($arr[$i], $arr[$i + 1]);
+            
+     
+        }
+        $greater = !$greater;
+    }
+    print_r($arr);
+}
+
+$data  = [1, 4, 3, 2];
+sortZigZag($data, count($data));
