1c6a7f6bb07e87f700c7c2564248b1333e478dae
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-01-27 19:04:32 +0530

    getDate() method added

--- Variables/Date/Date.php
+++ Variables/Date/Date.php
@@ -5,6 +5,29 @@ class DateOperation
     {
         return checkdate($mm, $dd, $yy);
     }
+    public function getData()
+    {
+        return new DateTime();
+    }
+    public function getTimeStamp()
+    {
+        $date = new DateTime();
+        return $date->getTimestamp();
+    }
+    public function getCurrentTimeStamp()
+    {
+        return time();
+    }
 }
 $dateOpeartion = new DateOperation();
-var_dump($dateOpeartion->checkDate(12,12,99));
+// var_dump($dateOpeartion->checkDate(12, 12, 99));
+// var_dump($dateOpeartion->getTimeStamp());
+$date = new DateTime();
+// echo $date->format("j-M-y h:mm:s , H:i: (D)");
+
+$timestamp = $date->format('U');
+// echo $timestamp;
+// echo $dateOpeartion->getCurrentTimeStamp();
+
+echo date('D-m-Y',strtotime('today'));
+
