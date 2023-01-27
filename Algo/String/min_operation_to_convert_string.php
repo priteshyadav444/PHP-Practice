dfd440b41ffc0087692344975e0465aa7ab8e227
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-01-27 17:02:22 +0530

    min operation to Convert string program

--- Algo/String/min_operation_to_convert_string.php
+++ Algo/String/min_operation_to_convert_string.php
@@ -1,16 +1,18 @@
 <?php
 //https://practice.geeksforgeeks.org/problems/edit-distance3702/1?page=1&status[]=solved&category[]=Strings&curated[]=1&sortBy=submissions
+$dp = array();
 function solve($str1, $str2, $length1, $length2)
 {
     if ($length1 == 0)
-        return $length2;
+        $dp[$length1][$length2] = $length2;
     if ($length2 == 0)
-        return $length1;
+        $dp[$length1][$length2] = $length1;
+
 
     if ($str1[$length1 - 1] == $str2[$length2 - 1])
-        return solve($str1, $str2, $length1 - 1, $length2 - 1);
+        $dp[$length1][$length2] = solve($str1, $str2, $length1 - 1, $length2 - 1);
 
-    return 1 + min(solve($str1, $str2, $length1 - 1, $length2), solve($str1, $str2, $length1 - 1, $length2 - 1), solve($str1, $str2, $length1, $length2 - 1));
+    return 1 + min($dp[$length1 - 1][$length2], $dp[$length1 - 1][$length2 - 1], $dp[$length1][$length2 - 1]);
 }
 
 $str1 = 'sunday';
