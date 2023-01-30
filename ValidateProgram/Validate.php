c6f2935237b9e3dbc949ac5c06e4fb8a96a53495
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-01-30 20:04:27 +0530

    Validator class updated

--- ValidateProgram/Validate.php
+++ ValidateProgram/Validate.php
@@ -19,11 +19,82 @@ class ErrorHandler extends ObjectFormatter
     private $errors = array();
     public function setError($newError, $key)
     {
-        array_push($this->errors, array($key => $newError));
+        if (!empty($key)) {
+            if (isset($this->errors[$key]) && is_array($this->errors[$key])) {
+                array_push($this->errors[$key], $newError);
+            } else {
+                $this->errors[$key]  = array($newError);
+            }
+        }
     }
+
     public function all()
     {
-        return $this->errors;
+        $allErrors = array();
+        foreach ($this->errors as $keyError) {
+            foreach ($keyError as $error)
+                array_push($allErrors, $error); {
+            }
+        }
+        return $allErrors;
+    }
+    // return is errors present for all as well as particular key
+    public function isError($key = null): bool
+    {
+        if (empty($this->errors)) return false;
+
+        if ($key == null) {
+            return count($this->errors);
+        } else {
+            if (isset($this->errors[$key]))
+                return count($this->errors[$key]);
+        }
+        return false;
+    }
+    // display error message mapped Error code
+    // @params: $errorCode : error Code of Error Message
+    // @return type : void
+    public function errorHandler($errorCode, $dataTypes = null, $keys = "", $return = false)
+    {
+        $errorMessage = match ($errorCode) {
+            'INVALID_DATATYPE' => 'Enter Valid Data Expected',
+            'INVALID_DATATYPE_INT' => "$keys Must be $dataTypes",
+            'INVALID_DATATYPE_STRING' => "$keys Must be $dataTypes",
+            'INVALID_OPTION' => 'Enter option is In Valid',
+            'NO_DATA_FOUND' => 'No Record Found!!',
+            'DATABASE_EMPTY' => 'Dataset is Empty!!!!',
+            'FIELD_REQUIRED' => "$keys field required",
+            default => "Unexpected Error",
+        };
+        if ($return == true) {
+            return $errorMessage;
+        } else {
+            if ($errorCode == 'INVALID_DATATYPE' && $dataTypes != null) {
+                self::__displayError($errorMessage, $dataTypes);
+                return;
+            }
+            self::__displayError($errorMessage);
+        }
+    }
+    // display error message with expected Data Types 
+    // @params: $dataTypes : pass datatypes that  expected 
+    // @return type : void
+    private static function __displayError($message, $dataTypes = null)
+    {
+        $message = $dataTypes == null ? $message : $message . " Expectded ($dataTypes)";
+        self::echoit($message);
+    }
+    public function echoit($msg, $newLine = 2)
+    {
+        if ($newLine < 1) {
+            echo "$msg \n";
+            return;
+        }
+
+        $temp = "";
+        for ($i = 1; $i <= $newLine; $i++)
+            $temp .= "\n";
+        echo $msg . $temp;
     }
 }
 
@@ -52,6 +123,7 @@ class Validate extends ErrorHandler
     // @return type : boolean
     public static function isString($input): bool
     {
+        self::isEmpty($input);
         return is_string($input);
     }
     // Return integer, float or bool when invalid data 
@@ -126,8 +198,8 @@ class Validate extends ErrorHandler
         $result = Validate::$functionName($input);
         do {
             if ($result === false) {
-                self::__displayError($dataTypes);
-                self::errorHandler('INVALID_DATATYPE', $dataTypes);
+                // ErrorHandler::__displayError($dataTypes);
+                ErrorHandler::errorHandler('INVALID_DATATYPE', $dataTypes);
                 $result = Validate::$functionName(readline("Enter Input Again :\n"));
             } else {
                 break;
@@ -151,35 +223,37 @@ class Validate extends ErrorHandler
     // display error message o mapped Error code
     // @params: $errorCode : error Code of Error Message
     // @return type : void
-    public function errorHandler($errorCode, $dataTypes = null, $keys = "", $return = false)
-    {
-        $errorMessage = match ($errorCode) {
-            'INVALID_DATATYPE' => 'Enter Valid Data Expected',
-            'INVALID_OPTION' => 'Enter option is In Valid',
-            'NO_DATA_FOUND' => 'No Record Found!!',
-            'DATABASE_EMPTY' => 'Dataset is Empty!!!!',
-            'FIELD_REQUIRED' => "$keys Field Required ",
-            default => "Unexpected Error",
-        };
-        if ($return == true) {
-            return $errorMessage;
-        } else {
-            if ($errorCode == 'INVALID_DATATYPE' && $dataTypes != null) {
-                self::__displayError($errorMessage, $dataTypes);
-                return;
-            }
-            self::__displayError($errorMessage);
-        }
-    }
+    // public function errorHandler($errorCode, $dataTypes = null, $keys = "", $return = false)
+    // {
+    //     $errorMessage = match ($errorCode) {
+    //         'INVALID_DATATYPE' => 'Enter Valid Data Expected',
+    //         'INVALID_DATATYPE_INT' => "$keys Must be $dataTypes",
+    //         'INVALID_DATATYPE_STRING' => "$keys Must be $dataTypes",
+    //         'INVALID_OPTION' => 'Enter option is In Valid',
+    //         'NO_DATA_FOUND' => 'No Record Found!!',
+    //         'DATABASE_EMPTY' => 'Dataset is Empty!!!!',
+    //         'FIELD_REQUIRED' => "$keys field required",
+    //         default => "Unexpected Error",
+    //     };
+    //     if ($return == true) {
+    //         return $errorMessage;
+    //     } else {
+    //         if ($errorCode == 'INVALID_DATATYPE' && $dataTypes != null) {
+    //             self::__displayError($errorMessage, $dataTypes);
+    //             return;
+    //         }
+    //         self::__displayError($errorMessage);
+    //     }
+    // }
 
     // display error message with expected Data Types 
     // @params: $dataTypes : pass datatypes that  expected 
     // @return type : void
-    private function __displayError($message, $dataTypes = null)
-    {
-        $message = $dataTypes == null ? $message : $message . " Expectded ($dataTypes)";
-        self::echoit($message);
-    }
+    // private function __displayError($message, $dataTypes = null)
+    // {
+    //     $message = $dataTypes == null ? $message : $message . " Expectded ($dataTypes)";
+    //     self::echoit($message);
+    // }
     // echo with two line
     public function echoit($msg, $newLine = 2)
     {
@@ -204,8 +278,8 @@ class Validate extends ErrorHandler
         $data = htmlspecialchars($data);
         return $data;
     }
-    public function isEmpty($input): bool
+    public static function isEmpty($input): bool
     {
-        return empty($input);
+        return (strlen($input) == 0);
     }
 }
