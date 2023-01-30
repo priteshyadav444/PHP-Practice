4a00bbb570db46fb1ce580a1ebe890d38fa53afe
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-01-30 20:00:32 +0530

    prevention xss attack in php form action

--- FormHandling/index.php
+++ FormHandling/index.php
@@ -6,18 +6,43 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 </head>
+<script>
+    $(document).ready(function() {
+        const data = new FormData();
+        $("#submit").click(function() {
+            $.post('data_test.php'), {
+                    name: "Pritesh"
+                },
+                function(data, status) {
+                    $("#msg").load(data + status);
+                }
+        });
+    });
+</script>
 <?php
+
+
 if (isset($_REQUEST['submit'])) {
-    print_r(htmlspecialchars($_GET['name']));
+    echo "<pre>";
+    print_r(getallheaders());
+    echo "Request : ";
+    print_r($_REQUEST);
+    echo "GET : ";
+    print_r($_GET);
+    echo "POST : ";
+    print_r($_POST);
 }
 ?>
 
+
 <body>
-    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="GET">
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST" enctype="multipart/form-data">
         <input type="text" name="name" /> <br>
-        <input type="submit" name="submit" value="submit">
+        <input type="button" name="submit" id="submit" value="submit">
     </form>
+    <div id="msg"></div>
 </body>
 
 </html>
