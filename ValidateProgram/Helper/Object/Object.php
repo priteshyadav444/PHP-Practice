712a6dffd5df63b514137d20579a187ee220c64d
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-02-01 18:11:35 +0530

    object response fixed for message

--- ValidateProgram/Helper/Object/Object.php
+++ ValidateProgram/Helper/Object/Object.php
@@ -13,12 +13,18 @@ class ObjectFormatter
      * @param  mixed $statuscode
      * @return void
      */
-    public function format(array $data = [], int $code = 200, string $statuscode = "SUCCESSFULL", string $message = null)
+    public static function format(array $data = [], int $code = 200, string $statuscode = "SUCCESSFULL", string $message = null)
     {
-        $keys = array('statuscode', 'code', 'data');
-        $values = array($statuscode, $code, $data);
-        if ($message != null)
-            $keys["message"] = $message;
+        $keys = array();
+        $values = array();
+        if ($message != null) {
+            array_push($keys, 'statuscode', 'code', 'message', 'data');
+            array_push($values, $statuscode, $code, $message, $data);
+        } else {
+            $keys = array('statuscode', 'code', 'data');
+            $values = array($statuscode, $code, $data);
+        }
+
         return json_encode(array_combine($keys, $values));
     }
 }
