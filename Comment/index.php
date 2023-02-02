8abb8b718483c20f1c7ba0a54365d3483acd7ed9
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-02-02 10:54:54 +0530

    comment formet added

---
+++ Comment/index.php
@@ -0,0 +1,16 @@
+
+<?php
+/**
+ * validate : iterate through all passed validation keys afte senitizing data   
+ *
+ * @param  mixed $name : take refrences of data to sentize form data
+ * @param  mixed $age
+ * @return void
+ * @throws Exception Always
+ * @author Pritesh Yadav (priteshyadav444@gmail.com)
+ * @copyright MIT
+ * 
+ */
+function foo($name, $age)
+{
+}
