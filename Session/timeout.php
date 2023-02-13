b9a0a986797b1b20635e3b15e5ccd3da9b651ed5
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-02-13 17:37:32 +0530

    session program added

--- Session/timeout.php
+++ Session/timeout.php
@@ -1,15 +1,13 @@
 <?php
-    setcookie('myvar', "123", time()+3600, "/", "", "true");
-    session_start();
-    $_SESSION["myvar"] = "123";
-    
-    echo "<pre>";
-    var_export($_SESSION["myvar"]);
+setcookie('myvar', "123", time() + 3600, "/", "", "true");
+session_start();
+$_SESSION["myvar"] = "123";
 
-    echo "Session :";
-    print_r($_SESSION);
-    echo "Cookies :";
-    print_r($_COOKIE);
-    session_destroy();
+echo "<pre>";
+var_export($_SESSION["myvar"]);
 
-
+echo "Session :";
+print_r($_SESSION);
+echo "Cookies :";
+print_r($_COOKIE);
+session_destroy();
