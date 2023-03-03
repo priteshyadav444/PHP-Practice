5e5bb7755c9c46ae97ceaca8b3702367b3f6979b
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-03-03 14:37:05 +0530

    exception handling program added

---
+++ Exception/throw.php
@@ -0,0 +1,36 @@
+<?php
+//create function with an exception
+function checkNum($number)
+{
+    if ($number > 1) {
+        throw new Exception("Value must be 1 or below");
+    }
+    return true;
+}
+
+//trigger exception in a "try" block
+try {
+    checkNum(2);
+    //If the exception is thrown, this text will not be shown
+    echo 'If you see this, the number is 1 or below';
+}
+
+//catch exception
+catch (Exception $e) {
+    echo "<pre>";
+    print_r($e);
+    echo 'Message: ' . $e->getMessage();
+} finally {
+    echo PHP_EOL . "Finnaly";
+}
+
+class St
+{
+    public static function st()
+    {
+        return new static("Static Messages");
+    }
+}
+
+
+print_r(St::st());
