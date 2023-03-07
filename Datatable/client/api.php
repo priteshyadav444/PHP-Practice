9b3d229030224e4ff433b32d3d0827fca23ed822
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-03-07 15:33:18 +0530

    vlog api return value from cache refresh in 5 mn

--- Datatable/client/api.php
+++ Datatable/client/api.php
@@ -3,6 +3,8 @@ $tracker->start();
 
 use Tracker\Helper\DatabaseConfig;
 
+include "../../Redis/vendor/autoload.php";
+
 include 'connection.php';
 include 'ObjectFormat.php';
 
@@ -11,10 +13,21 @@ $con = new ConnectionLog_Dub($database);
 
 if (isset($_GET['logs'])) {
     if ($_GET['logs'] == "visitor") {
+        $redis = new Predis\Client();
+        $flag = true;
+        if ($redis->exists("result") && $flag == true) {
+            $data =  $redis->get("result");
+            print_r($data);
+            return;
+        } else {
+            $data = ObjectFormatter::format($con->getallVistorLog(), 200, "Success", "All Data Loaded");
+            $redis->set("result", $data);
+            $redis->expire("result", "300");
+        }
         header("content-type:application/json");
         http_response_code(200);
 
-        print_r(ObjectFormatter::format($con->getallVistorLog(), 200, "Success", "All Data Loaded"));
+        print_r($data);
     } else {
 
         header("content-type:application/json");
