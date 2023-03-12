1a248b5795cbb912dd39aa42b2cbcf13756ae6c3
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-03-12 10:02:50 +0530

    chat bot program added

---
+++ chatbot/home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Sendinblue Conversations {literal} -->
+<script>
+    (function(d, w, c) {
+        w.SibConversationsID = '640eccbf3a00011ba24f2727';
+        w[c] = w[c] || function() {
+            (w[c].q = w[c].q || []).push(arguments);
+        };
+        var s = d.createElement('script');
+        s.async = true;
+        s.src = 'https://conversations-widget.sendinblue.com/sib-conversations.js';
+        if (d.head) d.head.appendChild(s);
+    })(document, window, 'SibConversations');
+</script>
+<!-- /Sendinblue Conversations {/literal} -->
+</body>
+</html>
